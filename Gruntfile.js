c95f81e6fbb630302dd82e53abc91803f3130f87
Silas Caimi <silascaimi@live.com>
2020-08-11 10:41:04 -0300

    some changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,5 +58,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-mkdir');
+  
   grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
 };
